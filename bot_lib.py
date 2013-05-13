df1414d745e10129e5bbcff864240aab1dbea433
Jonas Augusto <jonas.agx@gmail.com>
2013-05-13 01:30:04 -0300

    Update bot_lib.py
    Fixed pep8 issues.

--- bot_lib.py
+++ bot_lib.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import codecs, json
-import wikipedia, catlib, pagegenerators
-import logging, os
+import codecs
+import json
+import wikipedia
+import catlib
+import pagegenerators
+import logging
+import os
 from time import sleep
 import settings
 import shelve
@@ -11,198 +15,226 @@ from datetime import datetime
 from urllib import quote
 from userlib import User
 
-logging.basicConfig(filename='logs/bot.log',level=logging.INFO)
+logging.basicConfig(filename='logs/bot.log', level=logging.INFO)
+
 
 class Collector:
-	def __init__(self, site, *categories):
-		self.log = True
-		self.categories = categories[0]
-		self.site = site
-		self.pages_set = set()
-		self.editors = {}
-		#self.folder = 'revisions/'
-		self.bot_set = self.get_bot_list()
+    def __init__(self, site, *categories):
+        self.log = True
+        self.categories = categories[0]
+        self.site = site
+        self.pages_set = set()
+        self.editors = {}
+        #self.folder = 'revisions/'
+        self.bot_set = self.get_bot_list()
 
-	def get_pages(self):
-		'''Retorna as páginas de uma dada categoria: category'''
-		#print type(self.categories)
-		for c in self.categories:
-			print c
-			cat = catlib.Category(self.site, c)
-			pages = pagegenerators.CategorizedPageGenerator(cat)
-			for page in pages:
-				if wikipedia.Page(self.site, page.title()).namespace() is 0:
-					self.pages_set.add(page.title())
+    def get_pages(self):
+        '''Retorna as páginas de uma dada categoria: category'''
+        #print type(self.categories)
+        for c in self.categories:
+            print c
+            cat = catlib.Category(self.site, c)
+            pages = pagegenerators.CategorizedPageGenerator(cat)
+            for page in pages:
+                if wikipedia.Page(self.site, page.title()).namespace() is 0:
+                    self.pages_set.add(page.title())
+        DAO('pages').insert_list(list(self.pages_set))
+        return list(self.pages_set)
 
-		DAO('pages').insert_list(list(self.pages_set))
-		
-		return list(self.pages_set)
+    def get_editors(self, start_time=None):
+        '''Get the revision history for a given page,
+        returns users list with:
+            -- User
+            -- Data/Timestamp
+        It's possible to select users based on edit timestamp,
+        just giving a start_time value in timestamp syntax.
+        '''
 
-	def get_editors(self, start_time=None):
-		'''Get the revision history for a given page,
-		returns users list with:
-			-- User
-			-- Data/Timestamp
-		It's possible to select users based on edit timestamp,
-		just giving a start_time value in timestamp syntax.
-		'''
+        if len(self.pages_set) is 0:
+            self.get_pages()
 
-		if len(self.pages_set) is 0:
-			self.get_pages()
+        if self.log is True:
+            print "There're %d pages to check historic" % len(self.pages_set)
 
-		if self.log is True:
-			print "There're %d pages to check historic" % len(self.pages_set)
+        for page in self.pages_set:
+            page = page.replace(" ", "_")
+            if self.log:
+                print "Get history for: ", page
+            try:
+                wpage = wikipedia.Page(self.site, page)
+            except Exception, e:
+                logging.info("Collector: Erro com página: " + page)
+                print "Página não existe - 404"
+                if self.log:
+                    print page
+                return list()
 
-		for page in self.pages_set:
-			page = page.replace(" ", "_")
-			if self.log:
-				print "Get history for: ", page
-			try:
-				wpage = wikipedia.Page(self.site, page)
-			except Exception, e:
-				logging.info("Collector: Erro com página: " + page)
-				print "Página não existe - 404"
-				if self.log:
-					print page
-				return list()
+            history = wpage.getLatestEditors(limit=100)
 
-			history = wpage.getLatestEditors(limit=100)
+            if start_time is None:
+                for h in history:
+                    if self.check_user(h['user']):
+                        if h['user'] in self.editors:
+                            self.editors[h['user']]['counter'] += 1
+                            self.editors[h['user']]['edits'].append(
+                                datetime.strptime(
+                                    h['timestamp'][0:10], "%Y-%m-%d"))
+                        else:
+                            self.editors[h['user']] = {'counter': 1,
+                             'invited':  False, 'edits': list()}
 
-			if start_time is None:
-				for h in history:
-					if self.check_user(h['user']):
-						if self.editors.has_key(h['user']):
-							self.editors[h['user']]['counter'] += 1
-							self.editors[h['user']]['edits'].append(datetime.strptime(h['timestamp'][0:10], "%Y-%m-%d"))
-						else:
-							self.editors[h['user']] = {'counter' : 1, 'invited' :  False, 'edits' : list()}
-							self.editors[h['user']]['edits'].append(datetime.strptime(h['timestamp'][0:10], "%Y-%m-%d"))
-			else:
-				for h in history:
-					if (start_time <= datetime.strptime(h['timestamp'][0:10],
-					 "%Y-%m-%d") and self.check_user(h['user'])):
-						if self.editors.has_key(h['user']):
-							self.editors[h['user']]['counter'] += 1
-							self.editors[h['user']]['edits'].append(datetime.strptime(h['timestamp'][0:10], "%Y-%m-%d"))
-						else:
-							self.editors[h['user']] = {'counter' : 1, 'invited' :  False, 'edits' : list()}
-							self.editors[h['user']]['edits'].append(datetime.strptime(h['timestamp'][0:10], "%Y-%m-%d"))
-			del history
-		return self.editors
+                            self.editors[h['user']]['edits'].append(
+                                datetime.strptime(
+                                    h['timestamp'][0:10], "%Y-%m-%d"))
+            else:
+                for h in history:
+                    if (start_time <= datetime.strptime(h['timestamp'][0:10],
+                        "%Y-%m-%d") and self.check_user(h['user'])):
 
-	def save(self, structure, name):
-		print "Salvando: ", name.encode('utf-8')
-		db = DAO(name.encode('utf-8'))
-		db.insert_dict(structure)
-		del db
+                        if h['user'] in self.editors:
+                            self.editors[h['user']]['counter'] += 1
+                            self.editors[h['user']]['edits'].append(
+                                datetime.strptime(
+                                    h['timestamp'][0:10], "%Y-%m-%d"))
+                        else:
+                            self.editors[h['user']] = {'counter': 1,
+                             'invited':  False, 'edits': list()}
+                            self.editors[h['user']]['edits'].append(
+                                datetime.strptime(
+                                    h['timestamp'][0:10], "%Y-%m-%d"))
+            del history
+        return self.editors
 
-	def check_user(self, username):
-		u = User(self.site, username)
-		if username is None or u.isAnonymous() or u.isBlocked():
-			return False
-		elif username in self.bot_set:
-			return False
-		if u.editCount() <= settings.edit_max_editcount and u.editCount() >= settings.edit_min_editcount:
-			return True
-		return False
+    def save(self, structure, name):
+        print "Salvando: ", name.encode('utf-8')
+        db = DAO(name.encode('utf-8'))
+        db.insert_dict(structure)
+        del db
 
-	def get_bot_list(self):
-		if os.path.isfile('bot_list.db') is True:
-			print "Bot list found: " + 'bot_list.db'
-			return DAO('bot_list').get_list()
-		else:
-			bot_set = []
-			cat = catlib.Category(self.site, u"!Robôs")
-			pages = pagegenerators.CategorizedPageGenerator(cat)
-			for page in pages:
-				if wikipedia.Page(self.site, page.title()).namespace() is 2:
-					bot_set.append(page.title().split(":")[1])
-			print u"A Wikipedia possui ", len(bot_set), u" robôs"
-			bots = DAO('bot_list')
-			bots.insert_list(bot_set)
-			del bots
-			return bot_set
+    def check_user(self, username):
+        ''' Check the editor status:
+        * How many editions were made
+        * Whether is a bot or is blocked
 
-	def load_editors(self, dbname):
-		db = DAO(dbname.encode('utf-8'))
-		self.editors = db.get_editors()
-		del db
+        There is a range of max and min number of editions the 
+        '''
+
+        u = User(self.site, username)
+        if username is None or u.isAnonymous() or u.isBlocked():
+            return False
+        elif username in self.bot_set:
+            return False
+        if (u.editCount() <= settings.edit_max_editcount
+         and u.editCount() >= settings.edit_min_editcount):
+            return True
+        return False
+
+    def get_bot_list(self):
+        if os.path.isfile('bot_list.db') is True:
+            print "Bot list found: " + 'bot_list.db'
+            return DAO('bot_list').get_list()
+        else:
+            bot_set = []
+            cat = catlib.Category(self.site, u"!Robôs")
+            pages = pagegenerators.CategorizedPageGenerator(cat)
+            for page in pages:
+                if wikipedia.Page(self.site, page.title()).namespace() is 2:
+                    bot_set.append(page.title().split(":")[1])
+            print u"A Wikipedia possui ", len(bot_set), u" robôs"
+            bots = DAO('bot_list')
+            bots.insert_list(bot_set)
+            del bots
+            return bot_set
+
+    def load_editors(self, dbname):
+        db = DAO(dbname.encode('utf-8'))
+        self.editors = db.get_editors()
+        del db
+
 
 class Invite:
-	def __init__(self, site, editors):
-		assert len(editors) > 0, "Nenhum editor foi passado"
-		self.editors = editors
-		self.site = site
-		self.invite = settings.invite_msg
-		self.user_discussion_page = settings.user_discussion_page
-		self.bot_comment = settings.bot_comment
+    def __init__(self, site, editors):
+        assert len(editors) > 0, "Nenhum editor foi passado"
+        self.editors = editors
+        self.site = site
+        self.invite = settings.invite_msg
+        self.user_discussion_page = settings.user_discussion_page
+        self.bot_comment = settings.bot_comment
 
-	def __init__(self, site, filename):
-		self.contact_users = settings.contact_users
-		self.contact_template = settings.contact_template
-		self.filename = filename
-		self.editors = DAO(filename)
-		self.site = site
-		self.invite = settings.invite_msg
-		self.user_discussion_page = settings.user_discussion_page
-		self.bot_comment = settings.bot_comment
+    def __init__(self, site, filename):
+        self.contact_users = settings.contact_users
+        self.contact_template = settings.contact_template
+        self.filename = filename
+        self.editors = DAO(filename)
+        self.site = site
+        self.invite = settings.invite_msg
+        self.user_discussion_page = settings.user_discussion_page
+        self.bot_comment = settings.bot_comment
 
-	def inviter(self):
-		for i, editor in enumerate(self.editors.get_editors()):
-			self.send(editor, self.contact_users[i%len(self.contact_users)])
-			sleep(1)
+    def inviter(self):
+        for i, editor in enumerate(self.editors.get_editors()):
+            self.send(editor, self.contact_users[i % len(self.contact_users)])
+            sleep(1)
 
-	def send(self, editor, contact):
-		try:
-			wpage = wikipedia.Page(self.site, self.user_discussion_page + quote(editor))
-			message = wpage.get() + self.invite % (self.contact_template % (contact, contact, contact))
-			print editor, self.invite % (self.contact_template % (contact, contact, contact))
-			wpage.put(message, comment=settings.bot_comment)
-			print editor, " convidado"
-			self.editors.db[editor]['invited'] = True
-			self.editors.db[editor]['invite_date'] = datetime.today()
-			self.editors.db.sync()
-		except Exception, e:
-			print "Erro com o usuário ", editor
-			logging.info("Invite: Erro ao convidar: " + editor)
-			#wpage.put(message, comment=self.bot_comment)
+    def send(self, editor, contact):
+        try:
+            wpage = wikipedia.Page(
+                self.site, self.user_discussion_page + quote(editor))
+            message = wpage.get() + self.invite % (
+                self.contact_template % (contact, contact, contact))
+
+            print editor, self.invite % (
+                self.contact_template % (contact, contact, contact))
+
+            wpage.put(message, comment=settings.bot_comment)
+            print editor, " convidado"
+            self.editors.db[editor]['invited'] = True
+            self.editors.db[editor]['invite_date'] = datetime.today()
+            self.editors.db.sync()
+        except Exception, e:
+            print "Erro com o usuário ", editor
+            logging.info("Invite: Erro ao convidar: " + editor)
+            #wpage.put(message, comment=self.bot_comment)
+
 
 class DAO:
-	'''	Database layer	'''
-	def __init__(self, dbname):
-		self.dbname = dbname
-		self.db = shelve.open(self.dbname+'.db', writeback=True)
-		self.query_buffer = None
+    ''' Database layer  '''
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.db = shelve.open(self.dbname+'.db', writeback=True)
+        self.query_buffer = None
 
-	def __del__(self):
-		self.db.close()
+    def __del__(self):
+        self.db.close()
 
-	def insert_dict(self, structure):
-		for key in structure:
-			self.db[key.encode('utf-8')] = structure[key]
-			print key, "inserted in db"
-		self.db.sync()
+    def insert_dict(self, structure):
+        for key in structure:
+            self.db[key.encode('utf-8')] = structure[key]
+            print key, "inserted in db"
+        self.db.sync()
 
-	def insert_list(self, structure):
-		self.db['pages'] = structure
-		self.db.sync()
+    def insert_list(self, structure):
+        self.db['pages'] = structure
+        self.db.sync()
 
-	def get_list(self):
-		return self.db.items()
+    def get_list(self):
+        return self.db.items()
 
-	def get_editors(self, threshold=1):
-		print "Searching editors with at least " + str(threshold) + " editions"
-		if self.query_buffer is not None:
-			if raw_input(
-				"Query buffer not empty, continue? [y/n]") is "n":
-				print "Query is not empty!"
-				#return None
+    def get_editors(self, threshold=1):
+        print "Searching editors with at least " 
+        +str(threshold) + " editions"
+        if self.query_buffer is not None:
+            response = raw_input(
+                "Query buffer not empty, continue? [y/n]")
+            if response is "n":
+                print "Query is not empty!"
 
-		self.query_buffer = []
+        self.query_buffer = []
 
-		for key in self.db:
-			if self.db[key]['counter'] >= threshold and not self.db[key]['invited']:
-				self.query_buffer.append(key)
+        for key in self.db:
+            if (self.db[key]['counter'] >= threshold
+             and not self.db[key]['invited']):
+                self.query_buffer.append(key)
 
-		print len(self.query_buffer), "para convidar"
-		return self.query_buffer
+        print len(self.query_buffer), "para convidar"
+        return self.query_buffer
